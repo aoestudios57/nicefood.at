b7579f43df2bcc3aaf9e153be9b39bcfc348f67e
aoestudios57 <107040470+aoestudios57@users.noreply.github.com>
2023-08-24 02:02:45 +0200

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+// Ersetze diese Konfigurationsdaten durch deine eigenen Firebase-Projektdaten
+const firebaseConfig = {
+    apiKey: "AIzaSyAqX15SQCHohB91DKV05JFiMlw423t4HL0",
+    authDomain: "nicefood-ebd41.firebaseapp.com",
+    databaseURL: "https://nicefood-ebd41-default-rtdb.firebaseio.com",
+    projectId: "nicefood-ebd41",
+    storageBucket: "nicefood-ebd41.appspot.com",
+    messagingSenderId: "413004406542",
+    appId: "1:413004406542:web:a24497bb31b1f26de9c15f"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  
