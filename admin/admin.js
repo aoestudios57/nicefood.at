7e6128be7eae6a7a8844db0dda987825d709fe11
aoestudios57 <107040470+aoestudios57@users.noreply.github.com>
2023-08-30 15:44:33 +0200

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,118 +1,114 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const ordersContainer = document.getElementById("orders");
-    const deleteAllButton = document.getElementById("deleteAllButton");
-  
-    const readyOrdersTable = document.getElementById("readyOrdersTable");
-    const readyOrdersBody = document.getElementById("readyOrdersBody");
-  
-    const firebaseConfig = {
-        apiKey: "AIzaSyAqX15SQCHohB91DKV05JFiMlw423t4HL0",
-        authDomain: "nicefood-ebd41.firebaseapp.com",
-        databaseURL: "https://nicefood-ebd41-default-rtdb.firebaseio.com",
-        projectId: "nicefood-ebd41",
-        storageBucket: "nicefood-ebd41.appspot.com",
-        messagingSenderId: "413004406542",
-        appId: "1:413004406542:web:a24497bb31b1f26de9c15f"
-      };
-  
-    firebase.initializeApp(firebaseConfig);
-  
-    const db = firebase.firestore();
-  
-    // Funktion zum Abrufen und Anzeigen von Bestellungen
-    function displayOrders() {
-      ordersContainer.innerHTML = "";
-      db.collection("orders").get()
-        .then(function(querySnapshot) {
-          const orders = [];
-          querySnapshot.forEach(function(doc) {
-            orders.unshift(doc.data());
-          });
-          orders.forEach(function(orderData) {
-            const orderElement = document.createElement("div");
-            orderElement.classList.add("order");
-            orderElement.innerHTML = `
-              <h3>Bestellung von ${orderData.name}</h3>
-              <p>Produkt: ${orderData.product}</p>
-              <p>Telefonnummer: ${orderData.phone}</p>
-              <p>Lieferadresse: ${orderData.address}</p>
-              <hr>
-            `;
-            ordersContainer.appendChild(orderElement);
-          });
-        })
-        .catch(function(error) {
-          console.error("Error getting documents: ", error);
+document.addEventListener("DOMContentLoaded", function () {
+  const ordersContainer = document.getElementById("orders");
+  const deleteAllButton = document.getElementById("deleteAllButton");
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAqX15SQCHohB91DKV05JFiMlw423t4HL0",
+    authDomain: "nicefood-ebd41.firebaseapp.com",
+    databaseURL: "https://nicefood-ebd41-default-rtdb.firebaseio.com",
+    projectId: "nicefood-ebd41",
+    storageBucket: "nicefood-ebd41.appspot.com",
+    messagingSenderId: "413004406542",
+    appId: "1:413004406542:web:a24497bb31b1f26de9c15f",
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  const db = firebase.firestore();
+
+  // Funktion zum Abrufen und Anzeigen von Bestellungen
+  function displayOrders() {
+    ordersContainer.innerHTML = "";
+    db.collection("orders")
+      .get()
+      .then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          const orderData = doc.data();
+          const orderElement = document.createElement("div");
+          orderElement.classList.add("order");
+
+          orderElement.innerHTML = `
+            <h3>Bestellung von ${orderData.name}</h3>
+            <p>Produkte:<br>${getFormattedOrderItemslist(orderData.items)}</p>
+            <p>Telefonnummer: ${orderData.phone}</p>
+            <p>Lieferadresse: ${orderData.address}</p>
+            <p>Bezirk: ${orderData.district}</p>
+            <p>Gesamtbetrag: ${orderData.total} Euro</p>
+            <button class="delete-button" data-order-id="${
+              doc.id
+            }">Löschen</button>
+            <hr>
+          `;
+          ordersContainer.appendChild(orderElement);
         });
-    }
-  
-    // Funktion zum Löschen aller Bestellungen
-    function deleteAllOrders() {
-      db.collection("orders").get()
-        .then(function(querySnapshot) {
-          querySnapshot.forEach(function(doc) {
-            doc.ref.delete();
+
+        const deleteButtons = document.querySelectorAll(".delete-button");
+        deleteButtons.forEach((button) => {
+          button.addEventListener("click", function () {
+            const orderId = button.getAttribute("data-order-id");
+            deleteOrder(orderId);
           });
-          displayOrders();
-        })
-        .catch(function(error) {
-          console.error("Error deleting documents: ", error);
         });
+      })
+      .catch(function (error) {
+        console.error("Error getting documents: ", error);
+      });
+  }
+
+  // Funktion zum Löschen einer Bestellung
+  function deleteOrder(orderId) {
+    db.collection("orders")
+      .doc(orderId)
+      .delete()
+      .then(function () {
+        displayOrders();
+      })
+      .catch(function (error) {
+        console.error("Error deleting order: ", error);
+      });
+  }
+
+  // Funktion zum Formatieren der Bestellungsartikel
+  function getFormattedOrderItems(items) {
+    return items
+      .map((item) => `${item.product} (${item.price} Euro) x ${item.quantity}`)
+      .join(", ");
+  }
+  function getFormattedOrderItemslist(items) {
+    return items
+      .map((item) => `${item.product} (${item.price} Euro) x ${item.quantity}`)
+      .join("<br>");
+  }
+
+  // Button-Eventlistener, um alle Bestellungen zu löschen
+  deleteAllButton.addEventListener("click", function () {
+    if (confirm("Möchtest du wirklich alle Bestellungen löschen?")) {
+      deleteAllOrders();
     }
-  
-    // Button-Eventlistener, um alle Bestellungen zu löschen
-    deleteAllButton.addEventListener("click", function() {
-      if (confirm("Möchtest du wirklich alle Bestellungen löschen?")) {
-        deleteAllOrders();
-      }
-    });
-  
-    // Funktion zum Abrufen und Anzeigen von bereiten Bestellungen
-    function displayReadyOrders() {
-      readyOrdersBody.innerHTML = "";
-      db.collection("readyOrders").get()
-        .then(function(querySnapshot) {
-          querySnapshot.forEach(function(doc) {
-            const orderData = doc.data();
-            const row = readyOrdersBody.insertRow();
-  
-            const nameCell = row.insertCell(0);
-            const productCell = row.insertCell(1);
-            const actionsCell = row.insertCell(2);
-  
-            nameCell.textContent = orderData.name;
-            productCell.textContent = orderData.product;
-  
-            const completeButton = document.createElement("button");
-            completeButton.textContent = "Bereit";
-            completeButton.addEventListener("click", function() {
-              db.collection("completedOrders").add(orderData)
-                .then(function() {
-                  doc.ref.delete();
-                  displayReadyOrders();
-                })
-                .catch(function(error) {
-                  console.error("Error moving order: ", error);
-                });
-            });
-  
-            actionsCell.appendChild(completeButton);
-          });
-        })
-        .catch(function(error) {
-          console.error("Error getting ready orders: ", error);
+  });
+
+  // Funktion zum Löschen aller Bestellungen
+  function deleteAllOrders() {
+    db.collection("orders")
+      .get()
+      .then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          doc.ref.delete();
         });
-    }
-  
-    // Initialisiere Anzeige
+        displayOrders();
+      })
+      .catch(function (error) {
+        console.error("Error deleting documents: ", error);
+      });
+  }
+
+  // Initialisiere Anzeige
+  displayOrders();
+  function refreshPage() {
     displayOrders();
     displayReadyOrders();
-    function refreshPage() {
-        displayOrders();
-        displayReadyOrders();
-      }
-      
-      // Starte regelmäßiges Aktualisieren alle 2 Sekunden
-      setInterval(refreshPage, 10000);
-  });
-  
+  }
+
+  // Starte regelmäßiges Aktualisieren alle 2 Sekunden
+  setInterval(refreshPage, 10000);
+});
