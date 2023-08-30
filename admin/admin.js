0af002afb5eea1a6f5454c5d025306eaa557e68c
aoestudios57 <107040470+aoestudios57@users.noreply.github.com>
2023-08-30 16:05:12 +0200

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,132 +1,101 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const productButtons = document.querySelectorAll(".product-button");
-  const cartItemsList = document.getElementById("cartItems");
-  const orderTotalDisplay = document.getElementById("orderTotal");
-  const submitButton = document.getElementById("submitOrder");
-  const orderSummary = document.getElementById("orderSummary");
-  const districtSelect = document.getElementById("district");
+  const ordersContainer = document.getElementById("orders");
+  const deleteAllButton = document.getElementById("deleteAllButton");
 
-  let cartItems = [];
-  let orderTotal = 0;
+  const firebaseConfig = {
+    apiKey: "AIzaSyAqX15SQCHohB91DKV05JFiMlw423t4HL0",
+    authDomain: "nicefood-ebd41.firebaseapp.com",
+    databaseURL: "https://nicefood-ebd41-default-rtdb.firebaseio.com",
+    projectId: "nicefood-ebd41",
+    storageBucket: "nicefood-ebd41.appspot.com",
+    messagingSenderId: "413004406542",
+    appId: "1:413004406542:web:a24497bb31b1f26de9c15f"
+  };
+    // Füge hier deine Firebase-Konfigurationsdaten ein
+  
 
-  productButtons.forEach((button) => {
-    button.addEventListener("click", function () {
-      const product = this.getAttribute("data-product");
-      const productPrice = getProductPrice(product);
+  firebase.initializeApp(firebaseConfig);
 
-      const existingCartItemIndex = cartItems.findIndex(
-        (item) => item.product === product
-      );
+  const db = firebase.firestore();
 
-      if (existingCartItemIndex !== -1) {
-        cartItems[existingCartItemIndex].quantity++;
-      } else {
-        cartItems.push({ product, price: productPrice, quantity: 1 });
-      }
-
-      orderTotal += productPrice;
-
-      updateCartDisplay();
-    });
-  });
-
-  function updateCartDisplay() {
-    cartItemsList.innerHTML = "";
-    cartItems.forEach((item, index) => {
-      const listItem = document.createElement("li");
-      listItem.textContent = `${item.product} (${item.price} Euro) x ${item.quantity}`;
-
-      const removeButton = document.createElement("button");
-      removeButton.textContent = "Entfernen";
-      removeButton.classList.add("remove-button");
-      removeButton.addEventListener("click", function () {
-        if (item.quantity > 1) {
-          item.quantity--;
-        } else {
-          cartItems.splice(index, 1);
-        }
-
-        orderTotal -= item.price;
-        updateCartDisplay();
-      });
-
-      listItem.appendChild(removeButton);
-      cartItemsList.appendChild(listItem);
-    });
-
-    orderTotalDisplay.textContent = `Gesamtpreis: ${orderTotal} Euro`;
-
-    const selectedDistrict = districtSelect.value;
-    if (selectedDistrict === "2" || selectedDistrict === "20") {
-      orderSummary.style.display = "block";
-      submitButton.disabled = false;
-    } else {
-      if (orderTotal >= 15) {
-        orderSummary.style.display = "block";
-        submitButton.disabled = false;
-      } else {
-        orderSummary.style.display = "block";
-        submitButton.disabled = true;
-      }
-    }
-  }
-
-  function getProductPrice(product) {
-    const productPrices = {
-      "Käse Baguette": 4,
-      "Extrawurst Baguette": 4,
-      "Wienerwurst Baguette": 4,
-      "Putenwurst Baguette": 4,
-      "All in One Baguette": 5,
-      // Weitere Produkte und Preise hier hinzufügen
-    };
-    return productPrices[product] || 0;
-  }
-
-  districtSelect.addEventListener("change", updateCartDisplay);
-
-  submitButton.addEventListener("click", function () {
-    const name = document.getElementById("name").value;
-    const phone = document.getElementById("phone").value;
-    const address = document.getElementById("address").value;
-    const selectedDistrict = districtSelect.value;
-
-    const orderDetails = {
-      items: cartItems,
-      name: name,
-      phone: phone,
-      address: address,
-      district: selectedDistrict,
-      total: orderTotal,
-    };
-
-    if (selectedDistrict === "2" || selectedDistrict === "20") {
-      // Kein Mindestbestellwert erforderlich
-      saveOrderToFirebase(orderDetails);
-    } else if (orderTotal < 15) {
-      alert("Der Mindestbestellwert beträgt 15 Euro.");
-    } else {
-      saveOrderToFirebase(orderDetails);
-    }
-  });
-
-  function saveOrderToFirebase(orderDetails) {
-    // Firebase initialisieren
-    firebase.initializeApp(firebaseConfig);
-
-    // Firestore-Referenz
-    const db = firebase.firestore();
-
+  // Funktion zum Abrufen und Anzeigen von Bestellungen
+  function displayOrders() {
+    ordersContainer.innerHTML = "";
     db.collection("orders")
-      .add(orderDetails)
-      .then(function (docRef) {
-        alert(
-          "Bestellung aufgegeben:\n\n Danke fürs Bestellen :) \n Für Details Rufnummer: 067761666158"
-        );
-        // Hier könntest du den Warenkorb leeren oder andere Aktionen durchführen
+      .get()
+      .then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          const orderData = doc.data();
+          const orderElement = document.createElement("div");
+          orderElement.classList.add("order");
+          orderElement.innerHTML = `
+            <h3>Bestellung von ${orderData.name}</h3>
+            <p>Produkte: ${getFormattedOrderItems(orderData.items)}</p>
+            <p>Telefonnummer: ${orderData.phone}</p>
+            <p>Lieferadresse: ${orderData.address}</p>
+            <p>Bezirk: ${orderData.district}</p>
+            <p>Gesamtbetrag: ${orderData.total} Euro</p>
+            <button class="delete-button" data-order-id="${doc.id}">Löschen</button>
+            <hr>
+          `;
+          ordersContainer.appendChild(orderElement);
+        });
+
+        const deleteButtons = document.querySelectorAll(".delete-button");
+        deleteButtons.forEach((button) => {
+          button.addEventListener("click", function () {
+            const orderId = button.getAttribute("data-order-id");
+            deleteOrder(orderId);
+          });
+        });
       })
       .catch(function (error) {
-        console.error("Fehler beim Speichern der Bestellung: ", error);
+        console.error("Error getting documents: ", error);
       });
   }
+
+  // Funktion zum Löschen einer Bestellung
+  function deleteOrder(orderId) {
+    db.collection("orders")
+      .doc(orderId)
+      .delete()
+      .then(function () {
+        displayOrders();
+      })
+      .catch(function (error) {
+        console.error("Error deleting order: ", error);
+      });
+  }
+
+  // Funktion zum Formatieren der Bestellungsartikel
+  function getFormattedOrderItems(items) {
+    return items
+      .map((item) => `${item.product} (${item.price} Euro) x ${item.quantity}`)
+      .join(", ");
+  }
+
+  // Button-Eventlistener, um alle Bestellungen zu löschen
+  deleteAllButton.addEventListener("click", function () {
+    if (confirm("Möchtest du wirklich alle Bestellungen löschen?")) {
+      deleteAllOrders();
+    }
+  });
+
+  // Funktion zum Löschen aller Bestellungen
+  function deleteAllOrders() {
+    db.collection("orders")
+      .get()
+      .then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          doc.ref.delete();
+        });
+        displayOrders();
+      })
+      .catch(function (error) {
+        console.error("Error deleting documents: ", error);
+      });
+  }
+
+  // Initialisiere Anzeige
+  displayOrders();
 });
